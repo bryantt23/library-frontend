414dbbbd13348194b4fb87d13db74b802b12191a
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-09 03:33:44 -0800

    8.18 Log in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import Authors from './components/Authors';
 import Books from './components/Books';
 import NewBook from './components/NewBook';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useApolloClient } from '@apollo/client';
+import LoginForm from './components/LoginForm';
 
 const ALL_AUTHORS = gql`
   query {
@@ -26,10 +27,51 @@ const ALL_BOOKS = gql`
   }
 `;
 
+
 const App = () => {
+  const [token, setToken] = useState(null)
   const [page, setPage] = useState('authors');
+  const [errorMessage, setErrorMessage] = useState("")
   const { loading: authorsLoading, error: authorsError, data: allAuthorsData } = useQuery(ALL_AUTHORS);
   const { loading: booksLoading, error: booksError, data: allBooksData } = useQuery(ALL_BOOKS);
+  const client = useApolloClient()
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  const Notify = ({ errorMessage }) => {
+    if (!errorMessage) {
+      return null
+    }
+    return (
+      <div style={{ color: 'red' }}>
+        {errorMessage}
+      </div>
+    )
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
+  }
 
   if (authorsLoading || booksLoading) {
     return <p>Loading...</p>;
@@ -48,6 +90,7 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={logout}>logout</button>
       </div>
 
       <Authors show={page === 'authors'} allAuthors={allAuthors} ALL_AUTHORS={ALL_AUTHORS} />
